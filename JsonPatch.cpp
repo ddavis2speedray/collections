a2b60ffeb3f42e64cffa7dea3698718ce852c707
Don Davis <don@d2clouds.com>
2015-05-11 14:36:02 -0500

    Daily Commit

---
+++ JsonPatch.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Donald Lamar Davis II on 5/11/15.
+//
+
+#include "JsonPatch.h"
---
+++ JsonPatch.h
@@ -0,0 +1,14 @@
+//
+// Created by Donald Lamar Davis II on 5/11/15.
+//
+
+#ifndef COLLECTIONS_JSONPATCH_H
+#define COLLECTIONS_JSONPATCH_H
+
+namespace collections {
+    class JsonPatch {
+
+    };
+}
+
+#endif //COLLECTIONS_JSONPATCH_H
